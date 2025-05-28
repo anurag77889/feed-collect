f431987c64f9f2c2289809ef5ea51ffc802dae25
Anurag Swarnakar <anuragswarnakar626@gmail.com>
2025-05-28 13:06:30 +0530

    "Updated UI"

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,14 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="min-h-screen flex justify-center items-center flex-col gap-5">
-      <h1 className="text-xl font-bold">
-        Feed Collect - One Stop Solution for Feedback Collection
+    <div className="bg-amber-300 min-h-screen flex justify-center items-center flex-col gap-6 ">
+      <h1 className="flex flex-col text-7xl gap-5 text-shadow-black text-black font-mono">
+        <strong className="shadow-md">Want to Review?</strong>
+        <strong className="shadow-md">Use Feed Collect</strong>
       </h1>
       <Link
         href={"/login"}
-        className="bg-black text-white font-mono px-4 py-2 rounded-3xl cursor-pointer hover:bg-white hover:text-black transition hover:scale-90"
+        className="bg-black w-fit text-amber-300 font-mono font-sm px-10 py-3 rounded-3xl cursor-pointer hover:bg-amber-300 hover:text-black transition hover:scale-90 hover:font-sm hover:border-2 border-black"
       >
         Login
       </Link>
