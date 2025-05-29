6350ab4ebeafe3cf2a85234eb6d4c51260508883
Anurag Swarnakar <anuragswarnakar626@gmail.com>
2025-05-29 13:18:09 +0530

    "Integrated MongoDB"

---
+++ lib/mongodb.ts
@@ -0,0 +1,32 @@
+import { MongoClient } from "mongodb";
+
+
+const uri = process.env.MONGODB_URI as string;
+const options = {};
+
+let client;
+let clientPromise: Promise<MongoClient>;
+
+declare global {
+    // eslint-disable-next-line no-var
+    var _mongoClientPromise: Promise<MongoClient> | undefined;
+}
+
+if(!process.env.MONGODB_URI){
+    throw new Error("Please add your Mongo URI to .env.local")
+}
+
+if(process.env.NODE_ENV === 'development'){
+    // In development mode, use a global variable so the value is preserved
+    if(!global._mongoClientPromise){
+        client = new MongoClient(uri, options);
+        global._mongoClientPromise = client.connect();
+    }
+    clientPromise = global._mongoClientPromise;
+} else{
+    // In production, create a new client for every call
+    client = new MongoClient(uri, options);
+    clientPromise = client.connect();
+}
+
+export default clientPromise;
