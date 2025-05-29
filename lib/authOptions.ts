797c28a228a6580cc664e99bf332c6ecb8901986
Anurag Swarnakar <anuragswarnakar626@gmail.com>
2025-05-29 09:05:43 +0530

    "Added Google Authentication"

--- lib/authOptions.ts
+++ lib/authOptions.ts
@@ -1,25 +1,32 @@
-import { NextAuthOptions } from "next-auth"
-import GithubProvider from "next-auth/providers/github"
-
+import { NextAuthOptions } from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions: NextAuthOptions = {
-    providers:[
-        GithubProvider({
-            clientId: process.env.GITHUB_ID!,
-            clientSecret: process.env.GITHUB_SECRET!,
-        })
-    ],
-    pages:{
-        signIn: "/login"
-    },
-    secret: process.env.NEXTAUTH_SECRET,
-    session:{
-        strategy: "jwt",
-        maxAge: 30*60,
-        updateAge: 5 * 60,
-    },
-    jwt:{
-        maxAge: 30*60,
-    }
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID!,
+      clientSecret: process.env.GOOGLE_SECRET!,
 
-}
+    })
+    
+  ],
+  pages: {
+    signIn: "/login",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+  session: {
+    strategy: "jwt",
+    maxAge: 30 * 60, // 30 minutes
+    updateAge: 5 * 60, // Refresh token every 5 minutes
+  },
+  jwt: {
+    maxAge: 30 * 60, // Token life
+  },
+  
+  
+};
